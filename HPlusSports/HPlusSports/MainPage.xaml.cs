2f31302f9117c0fdd57965d3ab7e8ac9a18a7405
Sadman-Arif-Wamim <sadmanarifwamim11@gmail.com>
2021-12-11 19:12:27 +0600

    Initial Upload

---
+++ HPlusSports/HPlusSports/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using HPlusSports.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HPlusSports
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Item_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            Services.Product product = e.CurrentSelection.First() as Services.Product;
+            Navigation.PushAsync(new ProductDetail());
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var products = await ProductService.GetProductsAsync();
+            BindingContext = products;
+        }
+    }
+}
---
+++ HPlusSports/HPlusSports/ProductDetail.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HPlusSports
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductDetail : ContentPage
+    {
+        public ProductDetail()
+        {
+            InitializeComponent();
+        }
+        public ProductDetail(Services.Product product)
+        {
+            InitializeComponent();
+            BindingContext = product;
+        }
+        public void Handle_Clicked(object sender, EventArgs e)
+        {
+            Services.Product p = BindingContext as Services.Product;
+            Navigation.PushAsync(new OrderForm(
+                new Services.Order { ProductName = p.Name, Quantity=1}));
+        }
+    }
+}
